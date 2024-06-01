4f44c170b62ef78e22c20c466a9c03272ae7a9c1
Anton Kabanov <imcreativchik@gmail.com>
2024-06-02 02:03:49 +0500

    added router for users

---
+++ users_service/src/router.py
@@ -0,0 +1,35 @@
+from fastapi import APIRouter, Depends
+from fastapi_users import FastAPIUsers
+from src.auth import auth_backend
+from src.manager import get_user_manager
+from src.models import User
+from src.schemas import UserRead, UserCreate
+
+router = APIRouter()
+
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
+
+current_user = fastapi_users.current_user()
+
+router.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
+
+router.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
+
+@router.get("/protected-route")
+def protected_route(user: User = Depends(current_user)):
+    return f"Hello, {user.username}"
+
+@router.get("/unprotected-route")
+def unprotected_route():
+    return "Hello, anonym"
