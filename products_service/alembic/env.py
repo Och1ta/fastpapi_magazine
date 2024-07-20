8c5216d23a7d6b9f4c9b579dce20f17b38a5a130
Anton Kabanov <imcreativchik@gmail.com>
2024-07-20 18:23:00 +0500

    moved from src in app dir

--- products_service/alembic/env.py
+++ products_service/alembic/env.py
@@ -1,31 +1,30 @@
+import os
 from logging.config import fileConfig
 
+from alembic import context
+from dotenv import load_dotenv
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
-from alembic import context
+from app.models import Base
 
-from src.models import Base
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
+load_dotenv()
+
+db_user = os.getenv("DB_USER", "postgres")
+db_pass = os.getenv("DB_PASS", "postgres")
+db_host = os.getenv("DB_HOST", "db")
+db_port = os.getenv("DB_PORT", "5432")
+db_name = os.getenv("DB_NAME", "test")
+
 config = context.config
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
+config.set_main_option('sqlalchemy.url', f"postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}")
+
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
 
 
 def run_migrations_offline() -> None:
